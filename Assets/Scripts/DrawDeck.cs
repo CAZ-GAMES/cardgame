00f231a0e5c0786bd316feaefb0e53e0b87e065b
Christian Hernandez <christianhernandez@Christians-Mini.localdomain>
2025-08-02 23:44:42 -0500

    Added function to drawable deck for only pulling top card out

--- Assets/Scripts/DrawDeck.cs
+++ Assets/Scripts/DrawDeck.cs
@@ -2,29 +2,49 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 using Unity.VisualScripting;
+using System.Collections.Generic;
 
 public class DrawDeck : MonoBehaviour
 {
-    //     public void OnDrop(PointerEventData eventData)
-    //     {
-    //         print("What was dropped: " + eventData.pointerDrag);
-    //         if (this.transform.childCount > 0)
-    //         {
-    //             this.transform.GetChild(0).gameObject.SetActive(false);
-    //             eventData.pointerDrag.transform.GetComponent<Renderer>().sortingOrder = 1;
-    //             eventData.pointerDrag.transform.SetParent(this.transform);
-    //             eventData.pointerDrag.gameObject.GetComponent<Drag>().posToReturnTo = this.transform.position;
-    //         }
-    //         else
-    //         {
-    //             eventData.pointerDrag.transform.GetComponent<Renderer>().sortingOrder = 1;
-    //             eventData.pointerDrag.transform.SetParent(this.transform);
-    //             eventData.pointerDrag.gameObject.GetComponent<Drag>().posToReturnTo = this.transform.position;
-
-    //         }
-    //     }
+    int pileCount;
+    [SerializeField]
+    // cover to fill in blank space of deck to make sure it look like cards are in there
+    private GameObject deckCover;
     void Update()
     {
-        print(this.transform.childCount);
+        // check to see if card has been pulled off of drawable deck
+        if (pileCount != transform.childCount && transform.childCount > 0)
+        {
+            Reactivate();
+        }
+    }
+
+    // set all cards inactive only first card to be active
+    public void SetCardsActive()
+    {
+        pileCount = transform.childCount;
+
+
+
+        while (pileCount > 0)
+        {
+            transform.GetChild(pileCount - 1).gameObject.SetActive(false);
+            pileCount--;
+        }
+        // set pileCount back to original number to be used in update method
+        pileCount = transform.childCount;
+        // activate the top card on deck to be pulled out
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    void Reactivate()
+    {
+        // active next top card
+        transform.GetChild(0).gameObject.SetActive(true);
+        // if the last card in deck then the deck cover should go away to show an empty deck
+        if (transform.childCount == 1)
+        {
+            deckCover.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 }
