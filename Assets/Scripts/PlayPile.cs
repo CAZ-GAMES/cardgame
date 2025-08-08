b79a96a5b870c68f33e1b478fecbb653de3dbad8
Osvaldo Castellanos <ocstllns@gmail.com>
2025-08-07 23:28:43 -0500

    only cards from the player hand can go on top of the play pile

--- Assets/Scripts/PlayPile.cs
+++ Assets/Scripts/PlayPile.cs
@@ -7,22 +7,34 @@ public class PlayPile : MonoBehaviour, IDropHandler
     int playPileSize = 0;
     public void OnDrop(PointerEventData eventData)
     {
-        print("What was dropped: " + eventData.pointerDrag);
-        if (this.transform.childCount > 0)
+        OnlyPlacePlayerHandCardInPlayPile(eventData);
+    }
+
+    public void OnlyPlacePlayerHandCardInPlayPile(PointerEventData eventData)
+    {
+        if (
+            (eventData.pointerDrag.gameObject.GetComponent<Drag>().originalParent.name == "Player Hand 0")
+            || (eventData.pointerDrag.gameObject.GetComponent<Drag>().originalParent.name == "Player Hand 1")
+            || (eventData.pointerDrag.gameObject.GetComponent<Drag>().originalParent.name == "Player Hand 2")
+        )
         {
-            print(this.transform.GetChild(playPileSize - 1).gameObject.name);
-            this.transform.GetChild(playPileSize - 1).gameObject.SetActive(false);
-            eventData.pointerDrag.transform.GetComponent<Renderer>().sortingOrder = 1;
-            eventData.pointerDrag.transform.SetParent(this.transform);
-            eventData.pointerDrag.gameObject.GetComponent<Drag>().posToReturnTo = this.transform.position;
-            playPileSize++;
-        }
-        else
-        {
-            eventData.pointerDrag.transform.GetComponent<Renderer>().sortingOrder = 1;
-            eventData.pointerDrag.transform.SetParent(this.transform);
-            eventData.pointerDrag.gameObject.GetComponent<Drag>().posToReturnTo = this.transform.position;
-            playPileSize++;
+            print("What was dropped: " + eventData.pointerDrag);
+            if (this.transform.childCount > 0)
+            {
+                print(this.transform.GetChild(playPileSize - 1).gameObject.name);
+                this.transform.GetChild(playPileSize - 1).gameObject.SetActive(false);
+                eventData.pointerDrag.transform.GetComponent<Renderer>().sortingOrder = 1;
+                eventData.pointerDrag.transform.SetParent(this.transform);
+                eventData.pointerDrag.gameObject.GetComponent<Drag>().posToReturnTo = this.transform.position;
+                playPileSize++;
+            }
+            else
+            {
+                eventData.pointerDrag.transform.GetComponent<Renderer>().sortingOrder = 1;
+                eventData.pointerDrag.transform.SetParent(this.transform);
+                eventData.pointerDrag.gameObject.GetComponent<Drag>().posToReturnTo = this.transform.position;
+                playPileSize++;
+            }
         }
     }
 }
