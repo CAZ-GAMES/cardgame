5a59e64133fea374d96763827652f4771c793a10
Christian Hernandez <teckiealwayz@gmail.com>
2025-08-22 02:17:33 -0500

    Starting to freeze cards in certain game states

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,21 +1,49 @@
+using System;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject PlayerFaceDown0;
+    GameObject[] PlayerFaceUpCards;
     [SerializeField]
-    GameObject PlayerFaceDown1;
+    GameObject[] PlayerFaceDownCards;
     [SerializeField]
-    GameObject PlayerFaceDown2;
+    GameObject[] PlayerHandCards;
+    [SerializeField]
+    GameObject[] CompFaceUpCards;
+    [SerializeField]
+    GameObject[] CompFaceDownCards;
+    [SerializeField]
+    GameObject[] CompHandCards;
+
+
     void OnEnable()
     {
         GameManager.OnGameStateChanged += FaceUpFreeze;
+        GameManager.OnGameStateChanged += UnlockPlayerCards;
     }
 
     void OnDisable()
     {
         GameManager.OnGameStateChanged -= FaceUpFreeze;
+        GameManager.OnGameStateChanged -= UnlockPlayerCards;
+    }
+
+    private void UnlockPlayerCards(GameState state)
+    {
+        if (state == GameState.PlayerReadyUp)
+        {
+            // Unlock Player Face Up Cards
+            for (int i = 0; i < PlayerFaceUpCards.Length; i++)
+            {
+                PlayerFaceUpCards[i].transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = true;
+            }
+            // Unlock Player Hand Cards
+            for (int i = 0; i < PlayerHandCards.Length; i++)
+            {
+                PlayerHandCards[i].transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
     }
 
     void FaceUpFreeze(GameState state)
@@ -23,9 +51,10 @@ public class CardManager : MonoBehaviour
         // player cards are frozen until endgame
         if (state == GameState.Ready)
         {
-            PlayerFaceDown0.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
-            PlayerFaceDown1.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
-            PlayerFaceDown2.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
+            for (int i = 0; i < PlayerFaceUpCards.Length; i++)
+            {
+                PlayerFaceUpCards[i].transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
+            }
         }
     }
 }
